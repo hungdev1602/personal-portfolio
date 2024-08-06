b7e616cc20f06e7386720d7bb51c7517176f44fa
Nguyen Dinh Hung <matrix16hung@gmail.com>
2024-08-06 23:15:22 +0300

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import logo from "../assets/logo.png"
 import { FaTelegram } from "react-icons/fa6";
-import { FaGithub, FaInstagram, FaVk } from "react-icons/fa";
+import { FaGithub, FaVk } from "react-icons/fa";
 
 const Navbar = () => {
   return (
@@ -9,10 +9,9 @@ const Navbar = () => {
         <img src={logo} alt="DH" className="w-full h-full object-cover mx-2" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaTelegram />
-        <FaGithub />
-        <FaInstagram />
-        <FaVk />
+        <a href="https://t.me/hungzxjee" target="_blank"><FaTelegram /></a>
+        <a href="https://github.com/hungdev1602" target="_blank"><FaGithub /></a>
+        <a href="https://vk.com/id704508052" target="_blank"><FaVk /></a>
       </div>
     </nav>
   )
